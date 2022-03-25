e08cd842bc710753939192c63fc5c4c60d21a126
Keith <default@invalid.com>
2022-03-25 09:32:49 -0400

    change SharedPreferences to work in background, because of possible anr's; also print subset of session key

--- app/src/main/java/com/example/login/data/SessionDataSource.java
+++ app/src/main/java/com/example/login/data/SessionDataSource.java
@@ -2,10 +2,22 @@ package com.example.login.data;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
+import com.b12kab.tmdblibrary.exceptions.TmdbException;
+import com.example.login.app.ThisApplication;
 import com.example.login.data.model.LoggedInSession;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import static com.b12kab.tmdblibrary.NetworkHelper.TmdbCodes.TMDB_CODE_EXCEPTION_UNKNOWN_CAUSE;
+import static java.lang.Thread.sleep;
+
 public class SessionDataSource {
+    private final static String TAG = SessionDataSource.class.getSimpleName();
+
     private static final String SHARED_PREFERENCE_EDITOR_NAME = "shared_pref_name";
     private static final String SHARED_PREFERENCE_KEY_SESSION_KEY = "session_id";
 
@@ -25,10 +37,20 @@ public class SessionDataSource {
 
     public Result<?> setSession(Context context, String tmdbSession) {
         try {
-            final SharedPreferences.Editor editor =
-                    context.getSharedPreferences(SHARED_PREFERENCE_EDITOR_NAME, Context.MODE_PRIVATE).edit();
-            editor.putString(SHARED_PREFERENCE_KEY_SESSION_KEY, tmdbSession);
-            editor.apply();
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    Log.i(TAG, "setResult - runnable - start");
+                    final SharedPreferences.Editor editor =
+                            context.getSharedPreferences(SHARED_PREFERENCE_EDITOR_NAME, Context.MODE_PRIVATE).edit();
+                    editor.putString(SHARED_PREFERENCE_KEY_SESSION_KEY, tmdbSession);
+                    editor.apply();
+                    Log.i(TAG, "setResult - runnable - finish");
+                }
+            };
+
+            // Note: apply's under certain conditions can cause an ANR on the UI thread, so run it on a different thread
+            this.setResult(runnable);
 
             return new Result.Success<Boolean>(true);
         } catch (Exception e) {
@@ -38,14 +60,53 @@ public class SessionDataSource {
 
     public Result<?> deleteSession(Context context) {
         try {
-            final SharedPreferences.Editor editor =
-                    context.getSharedPreferences(SHARED_PREFERENCE_EDITOR_NAME, Context.MODE_PRIVATE).edit();
-            editor.remove(SHARED_PREFERENCE_KEY_SESSION_KEY);
-            editor.apply();
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    Log.i(TAG, "deleteSession - runnable - start");
+                    final SharedPreferences.Editor editor =
+                            context.getSharedPreferences(SHARED_PREFERENCE_EDITOR_NAME, Context.MODE_PRIVATE).edit();
+                    editor.remove(SHARED_PREFERENCE_KEY_SESSION_KEY);
+                    editor.apply();
+                    Log.i(TAG, "deleteSession - runnable - finish");
+                }
+            };
+
+            // Note: apply's under certain conditions can cause an ANR on the UI thread, so run it on a different thread
+            this.setResult(runnable);
 
             return new Result.Success<Boolean>(true);
         } catch (Exception e) {
             return new Result.Error(e);
         }
+    }
+
+    private void setResult(Runnable runnable) throws Exception {
+        ExecutorService x = ThisApplication.getExecutor();
+
+        try {
+            Log.i(TAG, "setResult - about to submit runnable");
+            Future<?> future = x.submit(runnable);
+            future.get();
+            while(!future.isDone()) {
+                sleep(500);
+            }
+            Log.i(TAG, "setResult - runnable complete");
+        } catch (ExecutionException ee) {
+            if (ee.getCause() != null) {
+                if (ee.getCause() instanceof Exception )
+                    throw (Exception) ee.getCause();
+            }
+
+            TmdbException exception = new TmdbException();
+            exception.setCode(TMDB_CODE_EXCEPTION_UNKNOWN_CAUSE);
+            Log.i(TAG, "setResult - ExecutionException non Exception cause - so throw new TmdbException");
+            throw exception;
+        } catch (InterruptedException ie) {
+            Log.i(TAG, "setResult - InterruptedException");
+            int a = 5;
+        }
+
+        Log.i(TAG, "setResult - normal exit");
     }
 }
--- app/src/main/java/com/example/login/ui/loggedin/LoggedInViewModel.java
+++ app/src/main/java/com/example/login/ui/loggedin/LoggedInViewModel.java
@@ -7,6 +7,9 @@ import com.example.login.data.LoginRepository;
 import com.example.login.data.Result;
 import com.example.login.data.model.LoggedInSession;
 
+import java.util.Random;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -27,9 +30,36 @@ public class LoggedInViewModel extends ViewModel {
     public void getSession(Context context) {
         Result<?> result = loginRepository.getSession(context);
         if (result instanceof Result.Success) {
-            this.sessionUpdateResult.setValue(new SessionResult(((Result.Success<LoggedInSession>) result).getData()));
+            // for security only show a portion of the result
+            String fullSession = ((Result.Success<LoggedInSession>) result).getData().getTmdbSession();
+            String sessionSubset = getSessionSubset(fullSession);
+            LoggedInSession session = new LoggedInSession();
+            session.setTmdbSession(sessionSubset);
+            this.sessionUpdateResult.setValue(new SessionResult(session));
         } else {
             this.sessionUpdateResult.setValue(new SessionResult(R.string.session_get_failed));
         }
+    }
+
+    @NonNull
+    private String getSessionSubset(String session) {
+        String showSess = session;
+        if (session == null) {
+            return "<Empty>";
+        }
+
+        int len = session.length();
+        if (len > 0) {
+            int beginLen = new Random().nextInt(len);
+            int endLen = len -1;
+            if (len - beginLen > 10) {
+                endLen = beginLen + 10;
+            }
+            showSess = session.substring(beginLen, endLen);
+        } else {
+            showSess = "<Empty>";
+        }
+
+        return showSess;
     }
 }
